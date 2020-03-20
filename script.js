4c4149cfceb11e541985a2104ec3c134eae56e5d
Artemiy <GeekyNarciss@users.noreply.github.com>
2020-03-20 20:18:00 +0300

    fix: some changes for DOM task

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 //header
 function changeHeaderColor(item) {
-    anchors.forEach(item => item.style.color = "#fff");
-    item.style.color = "#f06c64";
+    anchors.forEach(item => item.style.color = '#fff');
+    item.style.color = '#f06c64';
   }
   
   function scrollToAnchor(item, index) {
+    const HEADER_HEIGHT = 40;
     if (index == 0) {
       window.scrollTo(0, 0);
     } 
@@ -23,13 +24,13 @@ function changeHeaderColor(item) {
     changeHeaderColor(item);
   }
   
-  let anchors = document.querySelectorAll("nav > a");
-  anchors[0].style.color = "#f06c64";
-  anchors.forEach((item, index) => item.addEventListener("click", () => scrollToAnchor(item, index)));
+  let anchors = document.querySelectorAll('nav > a');
+  anchors[0].style.color = '#f06c64';
+  anchors.forEach((item, index) => item.addEventListener('click', () => scrollToAnchor(item, index)));
 
 
 //slider
-function click_on_chev(){
+function clickOnChev(){
     if(slider_1){
         iphones.style = 'background: url(assets/slider-2.png);height: 513px;'
         iphone.forEach(elem => elem.style = 'display: none');
@@ -43,9 +44,9 @@ function click_on_chev(){
     }
 }
 
-function chlick_on_iphone(){
+function clickOnIphone(){
     let style_for_screen = 'background: #000';
-    let this_iphone = Array.from(this.getElementsByClassName('screen-iphone'))[0];
+    let this_iphone = Array.from(this.getElementsByClassName('iphone-screen'))[0];
     if(this_iphone.style.length){
         this_iphone.style = '';
     }else{
@@ -56,7 +57,7 @@ function chlick_on_iphone(){
 let iphones = Array.from(document.getElementsByClassName('iphones'))[0];
 let slider = Array.from(document.getElementsByClassName('slider'))[0];
 let iphone = Array.from(document.getElementsByClassName('iphone'));
-iphone.forEach(elem => elem.onclick = chlick_on_iphone)
+iphone.forEach(elem => elem.onclick = clickOnIphone)
 
 let slider_1 = true;
 
@@ -66,22 +67,22 @@ function changeTabColor(item) {
   if (item.disabled) return;
   tabs.forEach(item => {
     item.disabled = false;
-    item.style.color = "#767e9e";
-    item.style.borderColor = "#666d89";
+    item.style.color = '#767e9e';
+    item.style.borderColor = '#666d89';
   });
 
-  item.style.color = "#c5c5c5";
-  item.style.borderColor= "#c5c5c5";
+  item.style.color = '#c5c5c5';
+  item.style.borderColor= '#c5c5c5';
   item.disabled = true; 
 
   images.forEach((item, index) => {
     let current =  index - temp <= 0 ? index - temp + 12 : index - temp;
     item.src = `assets/image_${current}.svg`
     item.alt = `image_${current}`;
-    if (item.style.borderWidth == "5px") {
-      item.style.borderWidth = "0px";
-      item.style.left = "0px";
-      item.style.top = "0px";
+    if (item.style.borderWidth == '5px') {
+      item.style.borderWidth = '0px';
+      item.style.left = '0px';
+      item.style.top = '0px';
     }
   });
   temp ++;
@@ -89,33 +90,64 @@ function changeTabColor(item) {
 }
 
 function changeImageBorder(item) {
-  if (item.style.borderWidth != "5px") {
+  if (item.style.borderWidth != '5px') {
     images.forEach(item => {
-      item.style.borderWidth = "0px";
-      item.style.left = "0px";
-      item.style.top = "0px";
+      item.style.borderWidth = '0px';
+      item.style.left = '0px';
+      item.style.top = '0px';
     });
     
-    if (item.style.borderColor != "#F06C64") {
-    item.style.border = "5px solid #F06C64";
-    item.style.left = "-5px";
-    item.style.top = "-5px";
+    if (item.style.borderColor != '#F06C64') {
+    item.style.border = '5px solid #F06C64';
+    item.style.left = '-5px';
+    item.style.top = '-5px';
     }
   }
   else {
-    item.style.borderWidth = "0px";
-    item.style.left = "0px";
-    item.style.top = "0px";
+    item.style.borderWidth = '0px';
+    item.style.left = '0px';
+    item.style.top = '0px';
   }
 }
 
-let tabs = document.querySelectorAll("#portfolio > div > ul > li");
+let tabs = document.querySelectorAll('#portfolio > div > ul > li');
 let temp = 0; 
-tabs[0].style.color = "#c5c5c5";
-tabs[0].style.borderColor = "#c5c5c5";
-tabs.forEach(item => item.addEventListener("click", () => changeTabColor(item)));
-let images = document.querySelectorAll("#portfolio > div > div > img");
+tabs[0].style.color = '#c5c5c5';
+tabs[0].style.borderColor = '#c5c5c5';
+tabs.forEach(item => item.addEventListener('click', () => changeTabColor(item)));
+let images = document.querySelectorAll('#portfolio > div > div > img');
 images.forEach(item => {
-  item.style.position = "relative";
-  item.addEventListener("click", () => changeImageBorder(item))
+  item.style.position = 'relative';
+  item.addEventListener('click', () => changeImageBorder(item));
 });
+
+
+//get-a-quote
+const FORM = document.getElementById('form-elem');
+const FORMSUBJECT = document.querySelector('.input-subject');
+const FORMDESC = document.querySelector('.input-desc');
+const MODAL = document.querySelector('.modal-overlay');
+
+FORM.addEventListener('submit', (event) => {
+    event.preventDefault();
+    MODAL.hidden = false;
+
+    if (!FORMSUBJECT.value) MODAL.querySelector('.modal p:nth-child(2)').insertAdjacentHTML('beforeend', '<span id = "theme-in-modal">Без темы</span>');
+    else {
+        MODAL.querySelector('.modal p:nth-child(2)').insertAdjacentHTML('beforeend', `<span id = "theme-in-modal"><strong>Тема: </strong>${FORMSUBJECT.value}</span>`);
+    }
+
+    if (!FORMDESC.value) MODAL.querySelector('.modal p:nth-child(3)').insertAdjacentHTML('beforeend', '<span id = "desc-in-modal">Без описания</span>');
+    else {
+        MODAL.querySelector('.modal p:nth-child(3)').insertAdjacentHTML('beforeend', `<span id = "desc-in-modal"><strong>Описание: </strong>${FORMDESC.value}</span>`);
+    }
+});
+
+const OKBTN = document.getElementById('ok-btn');
+
+OKBTN.addEventListener('click', function(event) {
+    MODAL.hidden = true;
+    MODAL.querySelector('#theme-in-modal').remove();
+    MODAL.querySelector('#desc-in-modal').remove();
+    FORM.reset();
+})
